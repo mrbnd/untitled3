ac78dfbab785dd6d90cae5f0f06f8c919bfd4d41
Admin <Virusolog92@mail.ru>
2020-06-04 23:17:13 +0700

    Second class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello git");
-        ///System.out.println("Bye git");
+        //System.out.println("Bye git");
         System.out.println("Bye git");
     }
 }
