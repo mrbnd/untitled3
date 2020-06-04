60f00595328e39851cbe8a179bd50e346f4e5e32
Admin <Virusolog92@mail.ru>
2020-06-04 23:10:09 +0700

    Second iteration

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello git");
         ///System.out.println("Bye git");
+        System.out.println("Bye git");
     }
 }
